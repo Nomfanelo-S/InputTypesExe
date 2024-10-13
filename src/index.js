fc5c190166e7b67817a6112e3d5e883fbe519be7
Nomfanelo-S <nomfaneloskhosana2@gmail.com>
2024-10-13 05:17:11 +0200

    Made changes to JS
    Changed JS and edited Index

--- src/index.js
+++ src/index.js
@@ -1,21 +1,9 @@
 function changeTimezone(event) {
-  let parisTime = moment()
-    .tz("Europe/Paris")
-    .format("dddd, MMMM D, YYYY h:m A");
-  let sydneyTime = moment()
-    .tz("Australia/Sydney")
-    .format("dddd, MMMM D, YYYY h:m A");
-  let johannesburgTime = moment()
-    .tz("Africa/Johannesburg")
-    .format("dddd, MMMM D, YYYY h:m A");
-  if (event.target.value === "paris") {
-    alert(`It is ${parisTime} in Paris, France`);
-  }
-  if (event.target.value === "sydney") {
-    alert(`It is ${sydneyTime} in Sydney, Australia`);
-  }
-  if (event.target.value === "johannesburg") {
-    alert(`It is ${johannesburgTime} in Johannesburg, South Africa`);
+  if (event.target.value.length > 0) {
+    let currentTime = moment()
+      .tz(event.target.value)
+      .format("dddd, MMMM D, YYYY h:mm A");
+    alert(`It is ${currentTime} in ${event.target.value}`);
   }
 }
 
